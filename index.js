49143579e8c9c1d21360693098fc69240ac46a75
Robert Kojima <kojima.robert@gmail.com>
2017-10-19 15:30:58 -0700

    implementing hover on images

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 function showLinks() {
-    let newLinks = document.getElementsByClassName('links');
+    // newLinks isn't quite an array, so needs to be somewhat typecasted
+    const newLinks = Array.from(document.getElementsByClassName('links'));
+    // Using forEach rather than map b/c not returning anything
     if (newLinks[0].classList.contains('showLinks')) {
-        for (let i = 0; i < newLinks.length; i++) {
-            newLinks[i].classList.remove('showLinks');
-        }
+        newLinks.forEach(function(link) {
+            link.classList.remove('showLinks');
+        });
     } else {
-        for (let i = 0; i < newLinks.length; i++) {
-            newLinks[i].classList.add('showLinks');
-        }
+        newLinks.forEach(function(link) {
+            link.classList.add('showLinks');
+        });
     }   
 }
